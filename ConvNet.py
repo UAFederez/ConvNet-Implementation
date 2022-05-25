97d3d1ba742c352faa9d438c5b7bc8d0ce593ec1
Uriel Federez <uafederez@gmail.com>
2022-05-25 13:52:43 +0800

    Updated gradient calcualtion on pooling layers

--- ConvNet.py
+++ ConvNet.py
@@ -119,8 +119,14 @@ class ConvNet:
 
 class PoolLayer(Layer):
     def __init__(self, filter_size = 2, stride = 2, mode = 'avg'):
+        """ 
+
+        :param filter_size: The size of the filter (height and width)
+        :param stride: The amount to shift the sliding window on both axes
+        :param mode: Type of pooling used, must be one of 'avg' or 'max'
+        """
         self.mode = mode
-        assert self.mode in ['avg']
+        assert self.mode in ['avg', 'max']
 
         self.filter_size = filter_size
         self.stride = stride
@@ -157,15 +163,31 @@ class PoolLayer(Layer):
             strides = self.conv_strides,
         )
         
-        self.pooled = np.average(self.conv_windows, axis = (4, 5))
+        if self.mode == 'avg':
+            self.pooled = np.average(self.conv_windows, axis = (4, 5))
+        else:
+            self.pooled = self.conv_windows.max(axis = (4, 5))
         return self.pooled
     
     def backward(self, dL_da):
-        self.dZ_next  = np.full(self.input_batch.shape, 1.0 / (self.filter_size ** 2))
-        self.dZ_next *= np.tile(dL_da, (1, 1, self.stride, self.stride))
+        if self.mode == 'max':
+            # For max pooling dL_dz for any z in the input feature map is essentially the sum
+            # of dL_da for any a in the pooled map wherein z was the maximum element
+            self.da_dz  = np.where(self.conv_windows == np.expand_dims(self.pooled, axis = (4, 5)), 1.0, 0.0) * np.expand_dims(dL_da, axis = (4, 5))
+        else:
+            # For avg pooling, since each z in the input map contributes evenly to the average 
+            # for each receptive field, the dL_da for the corresponding 'a' is distributed
+            self.da_dz = np.full(self.conv_windows.shape, 1.0 / (self.filter_size ** 2)) * np.expand_dims(dL_da, axis = (4, 5))
 
-        assert self.dZ_next.shape == self.input_batch.shape, "[Internal Error] Incorrect gradient calculation {}, {}".format(self.dZ_next.shape, self.pooled.shape)
-        return self.dZ_next
+        self.dL_dz         = np.zeros(self.input_batch.shape)
+        self.dL_dz_windows = np.lib.stride_tricks.as_strided(self.dL_dz, shape = self.window_shape, strides = self.conv_strides)
+
+        for i in range(self.window_shape[2]):
+            for j in range(self.window_shape[3]):
+                self.dL_dz_windows[:, :, i, j] += self.da_dz[:, :, i, j]
+        
+        assert self.dL_dz.shape == self.input_batch.shape, "[Internal Error] Incorrect gradient calculation {}, {}".format(self.dZ_next.shape, self.pooled.shape)
+        return self.dL_dz
 
 class FlattenLayer(Layer):
     def __init__(self):
@@ -182,6 +204,11 @@ class FlattenLayer(Layer):
 
 class DenseLayer(Layer):
     def __init__(self, num_units, activation = 'softmax'):
+        """ 
+
+        :param num_units: The number of units (neurons) in this layer
+        :param activation: The nonlinear activation function to be used, must be one of 'relu', 'sigmoid' or 'tanh'
+        """
         assert activation in ['softmax', 'relu', 'sigmoid', 'tanh'], 'Please select a supported activation function'
 
         self.num_units  = num_units
@@ -199,14 +226,20 @@ class DenseLayer(Layer):
             'tanh'   : lambda dL_da, a: dL_da * (1 - a ** 2),
         }
         
-
         self.weights = None
         self.biases  = np.zeros((num_units, 1))
         self.vdb     = np.zeros(self.biases.shape)
 
-    # input_batch: (2-D) -> (num_features, batch_size)
-    # output     : (2-D) -> (num_units, batch_size)
     def forward(self, input_batch):
+        """ 
+        Calculate the forward pass for a dense layer
+
+        :param input_batch: 
+            Batch of input images, 2D, [n_input, batch_size]
+            
+        :return:
+            Batch of layer activations, 2D, [n_units, batch_size]
+        """
         self.input_batch = input_batch
         self.batch_size  = input_batch.shape[1]
 
@@ -224,9 +257,20 @@ class DenseLayer(Layer):
         return self.a_out
 
     def backward(self, dL_da):
-        # since softmax is a vector valued function, i.e the output depends
-        # on all the components of the vector, dL_dz is calculated with a special
-        # case using the Jacobian
+        """ 
+        Calculate the backward pass for a dense layer
+
+        :param dL_da: 
+            Gradients of (L)oss w.r.t (a)ctivations in subsequent 
+            layer, 2D, [n_units, batch_size]
+
+        :return:
+            Gradients of the (L)oss w.r.t (a)ctivations in the next
+            layer backwards, 2D, [n_input, batch_size]
+        """
+        # Since softmax is a vector valued function, i.e the output depends
+        # on all the components of the vector, dL_dz is calculated case 
+        # using the Jacobian
         if self.activation_func == 'softmax':
             self.diags = np.einsum('kx,kw->xwk', self.a_out, np.identity(self.a_out.shape[0]))
             self.da_dz = np.einsum('zx,xk->xkz', -self.a_out, self.a_out.T) + self.diags
@@ -241,7 +285,7 @@ class DenseLayer(Layer):
         self.dw = np.dot(self.dL_dz, self.input_batch.T) / self.batch_size
         self.db = np.sum(self.dL_dz, axis = 1, keepdims = True) / self.batch_size  
 
-        self.dA_prev = np.dot(self.weights.T, self.dL_dz)  
+        self.dL_da_prev = np.dot(self.weights.T, self.dL_dz)  
     
         eta = self.optimization_params['momentum']
         lr  = self.optimization_params['learning_rate']
@@ -252,22 +296,52 @@ class DenseLayer(Layer):
         self.weights -= lr * self.vdw
         self.biases  -= lr * self.vdb
 
-        return self.dA_prev
+        return self.dL_da_prev
 
 class ConvLayer(Layer):
-    def __init__(self, num_filters, filter_size, stride = 1, mode = 'valid'):
+    def __init__(self, num_filters, filter_size, stride = 1, mode = 'valid', activation = 'relu'):
+        """ 
+
+        :param num_filters: The number of filters to use for this ConvLayer
+        :param filter_size: The size (height and width) for a filter
+        :param stride: The amount to shift the sliding window on both axes
+        :param mode: Whether or not the output will be padded to be the (same) shape or strictly smaller (valid)
+        :param activation: The nonlinear activation function to be used, must be one of 'relu', 'sigmoid' or 'tanh'
+        """
         self.num_filters = num_filters
         self.filter_size = filter_size
         self.stride = stride
-        self.mode   = mode
+        self.mode = mode
+
+        assert mode in ['valid', 'same'], "Please select a valid convolution mode"
+        assert activation in ['sigmoid', 'relu', 'tanh'], "Please select a supported activation function"
+
+        self.activation = {
+            'relu'   : lambda z : np.maximum(0, z),
+            'sigmoid': lambda z : 1 / (1 + np.exp(-z)),
+            'tanh'   : lambda z : np.tanh(z)
+        } [activation]
+
+        self.activation_prime = {
+            'relu'   : lambda a : (a > 0.0),
+            'sigmoid': lambda a : (a * (1 - a)),
+            'tanh'   : lambda a : (1 - (a ** 2))
+        } [activation]
 
         self.biases  = np.zeros((num_filters, 1))
         self.vdb     = np.zeros(self.biases.shape)
         self.filters = None
 
-    # Input batch: (4-D) -> (image_idx, channel, height, width)
-    # filters    : (4-D) -> (filter_idx, channel, height, width)
     def forward(self, input_batch):
+        """ 
+        Calculate the forward pass for a convolutional layer
+
+        :param input_batch: 
+            Batch of input images, 4D, [image_idx, channel, height, width]
+            
+        :return:
+            Batch of convolved images, 4D, [image_idx, n_filters, out_height, out_width]
+        """
         self.input_batch = input_batch
 
         self.orig_shape   = input_batch.shape
@@ -317,16 +391,24 @@ class ConvLayer(Layer):
         
         self.conv  = np.einsum('xchwij,fcij->xfhw', self.conv_windows, self.filters)
         self.conv  = np.repeat(np.expand_dims(self.biases, axis = (0, 2)), self.conv.shape[0], axis = 0) + self.conv
-        self.a_out = np.maximum(0, self.conv) # ReLU
-
-        #print('out shape:', self.a_out.shape)
+        self.a_out = self.activation(self.conv)
 
         return self.a_out
     
-    # dLdA  : (4-D) -> (image_idx, num_filter, out_h, out_w)
-    # dLdAp : (4-D) -> (image_idx, channel, in_h, in_w)
-    def backward(self, dLdA):
-        self.dLdZ = dLdA * (self.conv > 0.0)
+    
+    def backward(self, dL_da):
+        """ 
+        Calculate the backward pass for a convolutional layer
+
+        :param dL_da: 
+            Gradients of (L)oss w.r.t (a)ctivations in subsequent 
+            layer, 4D, [idx_in_batch, channels, out_height, out_width]
+
+        :return:
+            Gradients of the (L)oss w.r.t (a)ctivations in the next
+            layer backwards, 4D, [idx_in_batch, channels, in_height, in_width]
+        """
+        self.dL_dz = dL_da * self.activation_prime(self.a_out)
 
         self.dW_window_shape = (
             self.batch_size, 
@@ -342,31 +424,31 @@ class ConvLayer(Layer):
             strides = self.conv_strides,
         )
 
-        self.dF = np.einsum('xchwjk,xfjk->fchw', self.dW_windows, self.dLdZ) / self.batch_size
-        self.db = np.einsum('xfhw->f', self.dLdZ).reshape(-1, 1) / self.batch_size
+        self.dF = np.einsum('xchwjk,xfjk->fchw', self.dW_windows, self.dL_dz) / self.batch_size
+        self.db = np.einsum('xfhw->f', self.dL_dz).reshape(-1, 1) / self.batch_size
 
         # If stride is not 1, pad dZ with zeros -- in between elements -- in the
         # spatial dimensions i.e. the last 2 in dZ.shape
-        self.dZ_prev = self.dLdZ
+        self.dL_dz_prev = self.dL_dz
 
         if self.stride != 1 and self.mode == 'valid':            
-            pad_loc_y    = np.repeat(np.arange(self.dZ_prev.shape[2])[1::], 1)
-            pad_loc_x    = np.repeat(np.arange(self.dZ_prev.shape[3])[1::], 1)
+            pad_loc_y    = np.repeat(np.arange(self.dL_dz_prev.shape[2])[1::], 1)
+            pad_loc_x    = np.repeat(np.arange(self.dL_dz_prev.shape[3])[1::], 1)
             #print('before padding dzPrev', self.dZ_prev.shape, self.a_out.shape)
-            self.dZ_prev = np.insert(np.insert(self.dZ_prev, pad_loc_y, 0, axis = 2), pad_loc_x, 0, axis = 3)
+            self.dL_dz_prev = np.insert(np.insert(self.dL_dz_prev, pad_loc_y, 0, axis = 2), pad_loc_x, 0, axis = 3)
 
         # Pad dZ such that a valid convolution with a stride of 1 will result in
         # the same shape as the output of the previous layer
         source_h, source_w = self.orig_shape[2::]
 
-        num_pad_y = int(np.ceil((source_h - self.dZ_prev.shape[2] + self.filter_size - 1) / 2))
-        num_pad_x = int(np.ceil((source_w - self.dZ_prev.shape[3] + self.filter_size - 1) / 2))
+        num_pad_y = int(np.ceil((source_h - self.dL_dz_prev.shape[2] + self.filter_size - 1) / 2))
+        num_pad_x = int(np.ceil((source_w - self.dL_dz_prev.shape[3] + self.filter_size - 1) / 2))
 
         #print(num_pad_x, num_pad_y, source_h, source_w, self.dZ_prev.shape)
 
         paddings = ((0, 0), (0, 0), (num_pad_y, num_pad_y), (num_pad_x, num_pad_x))
-        self.padded_dZ_prev = np.pad(self.dZ_prev, paddings, mode = 'constant')
-        dZ_prev_strides = self.padded_dZ_prev.strides
+        self.padded_dL_da_prev = np.pad(self.dL_dz_prev, paddings, mode = 'constant')
+        dZ_prev_strides = self.padded_dL_da_prev.strides
 
         self.dZ_prev_strides = (
             dZ_prev_strides[0],      
@@ -382,8 +464,8 @@ class ConvLayer(Layer):
             self.filter_size, self.filter_size,
         )
 
-        self.dZ_prev_windows = np.lib.stride_tricks.as_strided(
-            self.padded_dZ_prev,
+        self.dL_da_prev_windows = np.lib.stride_tricks.as_strided(
+            self.padded_dL_da_prev,
             shape   = self.dZ_prev_windows_shape,
             strides = self.dZ_prev_strides
         )
@@ -392,7 +474,7 @@ class ConvLayer(Layer):
         self.rotated_filters = np.rot90(np.swapaxes(self.filters, 0, 1), 2, axes = (2, 3))
 
         # Convolve the padded dZ with the rotated filters
-        self.dZ_prev_conv = np.einsum('xfhwij,cfij->xchw', self.dZ_prev_windows, self.rotated_filters)
+        self.dL_da_prev_conv = np.einsum('xfhwij,cfij->xchw', self.dL_da_prev_windows, self.rotated_filters)
 
         eta = self.optimization_params['momentum']
         lr  = self.optimization_params['learning_rate']
@@ -403,4 +485,4 @@ class ConvLayer(Layer):
         self.filters -= lr * self.vdw
         self.biases  -= lr * self.vdb
 
-        return self.dZ_prev_conv
+        return self.dL_da_prev_conv
